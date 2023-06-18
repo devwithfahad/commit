516d5677a4d65a38e81abbe07aa1baa45cdd2ee8
devwithfahad <fahad_ashraf98@yahoo.com>
2023-06-18 16:38:31 +0500

    first commit

---
+++ listpractice.dart
@@ -0,0 +1,14 @@
+void main (){
+  List studentnames = ["fahad","arslan","latif"];
+  print (studentnames);
+  studentnames.insert(2,"ali");
+  studentnames.sort();
+  var reversedList = List.of(studentnames.reversed);
+  print(studentnames);
+  print (reversedList);
+
+
+
+  
+
+}
